f3737b78dddc6486ea5e168743363034568aa1ed
Philip Mdicullar <phillip.emdokolo@student.moringaschool.com>
2025-12-10 22:40:09 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"; 
+function App() {
+
+  return (
+    <>
+      <Routes>
+        
+      </Routes>
+    </>
+  )
+}
+
+export default App
