0e4f6330cf6ce4bbb170c1170f482ed87f63d4ee
Philip Mdicullar <phillip.emdokolo@student.moringaschool.com>
2026-04-16 16:33:59 +0300

    navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import './App.css'
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"; 
+import Navbar from './Pages/Navbar/Navbar';
+import Home from './Pages/Home/Home';
+import Projects from './Pages/Projects/Projects';
+import About from './Pages/About/About';
+import Contact from './Pages/Contact/Contact';
+
 function App() {
 
   return (
-    <>
-      <Routes>
-        
-      </Routes>
-    </>
+    <div className="app">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import Logo from '../../assets/logo.png'
+import Hamburger from '../../assets/hamburger.png'
+import './navbar.css'
 
 function Navbar() {
+  const [open, setOpen] = useState(false)
+
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <div className="logo">
+        <img src={Logo} alt="logo" />
+      </div>
+
+      <nav className={open ? "nav-links active" : "nav-links"}>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      <div className="hamburger" onClick={() => setOpen(!open)}>
+        <img src={Hamburger} alt="hamburger"/>
+      </div>
+    </div>
   )
 }
 
